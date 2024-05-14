98412abc1a38478a70a5b94a8ff4fc2bdf59cff2
Wyfi0 <wyfi08g@proton.me>
2024-05-13 22:34:39 -0400

    recompiled

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,11 +3,11 @@ fn main() {
     let mut divisor: f64 = 1.0;
     let mut result: f64 = 0.0;
 
-    for i in 0..1_000_000_i64 {
+    for i in 0..1_000_000_000_000i64 {
     
         let sub_result: f64 = 4.0 / divisor;
     
-        if i % 2 == 0 {
+       if i % 2 == 0 {
             result = result + sub_result;
         }
         else {
